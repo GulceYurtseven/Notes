2ddc848aa12ac8c5941275c71eab968335897186
Beşan Gülce Yurtseven <132767560+GulceYurtseven@users.noreply.github.com>
2025-01-12 18:04:13 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Notlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.basliktextBox1 = new System.Windows.Forms.TextBox();
+            this.nottextBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bttNew = new System.Windows.Forms.Button();
+            this.bttSave = new System.Windows.Forms.Button();
+            this.bttRead = new System.Windows.Forms.Button();
+            this.bttnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(18, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Not Başlığı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(68, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Not:";
+            // 
+            // basliktextBox1
+            // 
+            this.basliktextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.basliktextBox1.Location = new System.Drawing.Point(128, 50);
+            this.basliktextBox1.Name = "basliktextBox1";
+            this.basliktextBox1.Size = new System.Drawing.Size(212, 30);
+            this.basliktextBox1.TabIndex = 2;
+            // 
+            // nottextBox2
+            // 
+            this.nottextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nottextBox2.Location = new System.Drawing.Point(122, 107);
+            this.nottextBox2.Multiline = true;
+            this.nottextBox2.Name = "nottextBox2";
+            this.nottextBox2.Size = new System.Drawing.Size(330, 205);
+            this.nottextBox2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(458, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(314, 262);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // bttNew
+            // 
+            this.bttNew.BackColor = System.Drawing.Color.Coral;
+            this.bttNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bttNew.Location = new System.Drawing.Point(122, 336);
+            this.bttNew.Name = "bttNew";
+            this.bttNew.Size = new System.Drawing.Size(106, 39);
+            this.bttNew.TabIndex = 5;
+            this.bttNew.Text = "Yeni";
+            this.bttNew.UseVisualStyleBackColor = false;
+            this.bttNew.Click += new System.EventHandler(this.bttNew_Click);
+            // 
+            // bttSave
+            // 
+            this.bttSave.BackColor = System.Drawing.Color.Coral;
+            this.bttSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bttSave.Location = new System.Drawing.Point(234, 336);
+            this.bttSave.Name = "bttSave";
+            this.bttSave.Size = new System.Drawing.Size(106, 39);
+            this.bttSave.TabIndex = 6;
+            this.bttSave.Text = "Kaydet";
+            this.bttSave.UseVisualStyleBackColor = false;
+            this.bttSave.Click += new System.EventHandler(this.bttSave_Click);
+            // 
+            // bttRead
+            // 
+            this.bttRead.BackColor = System.Drawing.Color.Coral;
+            this.bttRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bttRead.Location = new System.Drawing.Point(346, 336);
+            this.bttRead.Name = "bttRead";
+            this.bttRead.Size = new System.Drawing.Size(106, 39);
+            this.bttRead.TabIndex = 7;
+            this.bttRead.Text = "Göster";
+            this.bttRead.UseVisualStyleBackColor = false;
+            this.bttRead.Click += new System.EventHandler(this.bttRead_Click);
+            // 
+            // bttnDelete
+            // 
+            this.bttnDelete.BackColor = System.Drawing.Color.Coral;
+            this.bttnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bttnDelete.Location = new System.Drawing.Point(458, 336);
+            this.bttnDelete.Name = "bttnDelete";
+            this.bttnDelete.Size = new System.Drawing.Size(106, 39);
+            this.bttnDelete.TabIndex = 8;
+            this.bttnDelete.Text = "Sil";
+            this.bttnDelete.UseVisualStyleBackColor = false;
+            this.bttnDelete.Click += new System.EventHandler(this.bttnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 390);
+            this.Controls.Add(this.bttnDelete);
+            this.Controls.Add(this.bttRead);
+            this.Controls.Add(this.bttSave);
+            this.Controls.Add(this.bttNew);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.nottextBox2);
+            this.Controls.Add(this.basliktextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox basliktextBox1;
+        private System.Windows.Forms.TextBox nottextBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bttNew;
+        private System.Windows.Forms.Button bttSave;
+        private System.Windows.Forms.Button bttRead;
+        private System.Windows.Forms.Button bttnDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notlar
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;//tablo oluşturma
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Başlık", typeof(String));//tablonun içine yazılacak başlık 1. sütun
+            table.Columns.Add("Not", typeof(String)); // 2. sütun
+            dataGridView1.DataSource = table; // oluşturulan tabloyu grind e attım
+
+            dataGridView1.Columns["Not"].Visible = true; // açıldığında notlar görünmesin
+            dataGridView1.Columns["Başlık"].Width = 100; // başlığı güzel yazma
+        }
+
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            basliktextBox1.Clear();
+            nottextBox2.Clear();
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(basliktextBox1.Text, nottextBox2.Text);//tabledaki satırlara textbox lardaki bilgileri yazma
+            basliktextBox1.Clear();
+            nottextBox2.Clear();
+        }
+
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex; // o anki index tıkladığın anda
+            if (index > -1)//elbet seçilmiştir
+            {
+                basliktextBox1.Text = table.Rows[index].ItemArray[0].ToString();//itemArry, bir dizi aracılığıyla bu satırın değerlerini ayarlamak veya almak için kullanabilirsiniz. 
+                nottextBox2.Text = table.Rows[index].ItemArray[1].ToString();//örneğin 2. satırdayız index=1 table ın 2. satırını itemarray yardımıyla note text box a yazma (bu sırada string e dödüştürdük)
+            }
+        }
+
+        private void bttnDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;// tıkladığın andaki index
+            table.Rows[index].Delete();
+        }
+    }
+}
